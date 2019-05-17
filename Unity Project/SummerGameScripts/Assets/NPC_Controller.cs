ab5b6132c3ed55d525faa557845dfdab967e2adb
Unknown <skyesladeut@outlook.com>
2019-05-16 22:25:19 -0600

    Working on Raccoon Character

--- Unity Project/SummerGameScripts/Assets/NPC_Controller.cs
+++ Unity Project/SummerGameScripts/Assets/NPC_Controller.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Events;
@@ -7,15 +6,19 @@ using UnityEngine.Events;
 public class NPC_Controller : MonoBehaviour
 {
     private NavMeshAgent _agent;
-    private Vector3 target, offset;
-    public Vector3Data Destination;
+    private Vector3 target;
+    public TransformData Destination01, Destination02;
     public GameObject Player;
     public StringData MovementType;
-    private Animator anim;
+    public Animator anim;
+    private float x, z;
+    private bool reached_dest, rotate_dest;
+    public ActionObject Reach_Dest;
+    private Quaternion FacingDirection;
 
     private void Start()
     {
-        offset.Set(.1f, .1f, .1f);
+        //offset.Set(.1f, .1f, .1f);
         _agent = GetComponent<NavMeshAgent>();
         _agent.destination = transform.position;
         anim = GetComponentInChildren<Animator>();
@@ -23,17 +26,99 @@ public class NPC_Controller : MonoBehaviour
 
     public void Move()
     {
+        Debug.Log("Move");
+        FacingDirection = Destination01.trans.rotation;
+        reached_dest = false;
+        if(anim == null)
+            anim = GetComponentInChildren<Animator>();
         anim.SetTrigger("Walk");
+        if(_agent ==  null)
+            _agent = GetComponent<NavMeshAgent>();
         StartCoroutine(MovementType.value);
     }
 
-    private IEnumerator Sideways()
+    private IEnumerator RandomWalkBetween()
+    {
+        while (MovementType.value == "RandomWalkBetween")
+        {
+            reached_dest = false;
+            rotate_dest = false;
+            target = transform.position;
+            target.x = Random.Range(Destination01.trans.position.x, Destination02.trans.position.x);
+            target.z = Random.Range(Destination01.trans.position.z, Destination02.trans.position.z);
+            anim.SetTrigger("Walk");
+            while (!reached_dest && MovementType.value == "RandomWalkBetween")
+            {
+                _agent.destination = target;
+                CheckDest(.1f);
+                //Debug.Log(target + " " + transform.position);
+                yield return new WaitForFixedUpdate();
+            }
+
+            while (!rotate_dest && MovementType.value == "RandomWalkBetween")
+            {
+                //Debug.Log(transform.rotation.y + " " + FacingDirection.y);
+                transform.rotation = Quaternion.Lerp(transform.rotation, FacingDirection, 2f * Time.deltaTime);
+                CheckRot();
+                yield return new WaitForFixedUpdate();
+            }
+
+            StopMovement("RandomWalkBetween");
+            yield return new WaitForSeconds(Random.Range(3,5));
+        }
+    }
+
+    private IEnumerator SidewaysX()
     {
         //StopMovement();
-        while (MovementType.value == "Sideways")
+        while (MovementType.value == "SidewaysX")
         {
             target = transform.position;
             target.x = Player.transform.position.x;
+            _agent.destination = target;
+            CheckDest(.1f);
+            while (reached_dest)
+            {
+                anim.ResetTrigger("Walk");
+                anim.SetTrigger("Idle");
+                CheckDest(.1f);
+                yield return new WaitForFixedUpdate();
+            }
+            yield return new WaitForFixedUpdate(); 
+        }
+    }
+
+    private IEnumerator SidewaysZ()
+    {
+        Debug.Log("SideWaysZ");
+        _agent.updateRotation = false;
+        while (MovementType.value == "SidewaysZ")
+        {
+            reached_dest = false;
+            anim.SetTrigger("Walk");
+            target = transform.position;
+            target.z = Player.transform.position.z;
+            _agent.destination = target;
+            CheckDest(.1f);
+            while (reached_dest)
+            {
+                anim.ResetTrigger("Walk");
+                anim.SetTrigger("Idle");
+                CheckDest(.1f);
+                yield return new WaitForFixedUpdate();
+            }
+            yield return new WaitForFixedUpdate(); 
+        }
+
+        _agent.updateRotation = true;
+        Debug.Log("End");
+    }
+
+    private IEnumerator FollowPlayer()
+    {
+        while (MovementType.value == "FollowPlayer")
+        {
+            target = Player.transform.position;
             _agent.destination = target;
             yield return new WaitForFixedUpdate();
         }
@@ -42,25 +127,56 @@ public class NPC_Controller : MonoBehaviour
     private IEnumerator GoToDest()
     {
         //StopMovement();
-        while ((transform.position != Destination.vector) && MovementType.value == "GoToDest")
+        reached_dest = false;
+        rotate_dest = false;
+        target = Destination01.trans.position;
+        while (!reached_dest && MovementType.value == "GoToDest")
         {
-            _agent.destination = Destination.vector;
+            _agent.destination = target;
+            CheckDest(.1f);
             yield return new WaitForFixedUpdate();
-            if (((transform.position.z <= (Destination.vector + offset).z) &&
-                 (transform.position.x <= (Destination.vector + offset).x))
-                && ((transform.position.z >= (Destination.vector - offset).z) &&
-                    (transform.position.x >= (Destination.vector - offset).x)))
-                break;
         }
-
-        Debug.Log("Reached Movement");
-        StopMovement();
+        while (!rotate_dest && MovementType.value == "RandomWalkBetween")
+        {
+            //Debug.Log(transform.rotation.y + " " + FacingDirection.y);
+            transform.rotation = Quaternion.Lerp(transform.rotation, FacingDirection, 2f * Time.deltaTime);
+            CheckRot();
+            yield return new WaitForFixedUpdate();
+        }
+        Debug.Log("Reached Destination");
+        StopMovement("RandomWalkBetween");
+        Reach_Dest.Action.Invoke();
     }
 
-    public void StopMovement()
+    public void StopMovement(string coroutineName)
     {      
-        StopAllCoroutines();
+        StopCoroutine(coroutineName);
         anim.ResetTrigger("Walk");
         anim.SetTrigger("Idle");
     }
+
+    private void CheckDest(float offset)
+    {
+        if(((transform.position.z <= (target.z + offset)) &&
+        (transform.position.x <= (target.x + offset)))
+        && ((transform.position.z >= (target.z - offset)) &&
+        (transform.position.x >= (target.x - offset))))
+            reached_dest = true;
+        else
+        {
+            reached_dest = false;
+        }
+    }
+
+    private void CheckRot()
+    {
+        if((transform.rotation.eulerAngles.y <= (FacingDirection.eulerAngles.y + 5f))
+           && (transform.rotation.eulerAngles.y >= (FacingDirection.eulerAngles.y - 5f)))
+            rotate_dest = true;
+        else
+        {
+            rotate_dest = false;
+        }
+    }
+    
 }
---
+++ Unity Project/SummerGameScripts/Assets/Scripts/NPCWalkObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class NPCWalkObject : MonoBehaviour
+{
+   public enum WalkTypes
+   {
+      walk1,
+      walk2,
+      walk3,
+      walk4
+   };
+
+   
+}
